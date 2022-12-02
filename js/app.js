59b4d5e9a8b471b23cb8bb648444d2e72716d2a7
Mosiiuk <mosiiuk.ivan@gmail.com>
2022-12-02 19:07:28 +0200

    Добробив адаптивну верстку

--- js/app.js
+++ js/app.js
@@ -19,37 +19,11 @@ $(function () {
     }
   }
 
-  // Smoots scroll
+  // Menu nav
 
-  $("[data-scroll]").on(click, function (event) {
+  $("#nav-toggle").on("click", function (event) {
     event.preventDefault();
 
-    let blockId = this.data("scroll");
-  });
-
-  // hamburger menu
-
-  const menu = document.querySelector(".nav");
-  const menuItems = document.querySelectorAll(".nav-link");
-  const hamburger = document.querySelector(".hamburger");
-  const closeIcon = document.querySelector(".closeIcon");
-  const menuIcon = document.querySelector(".menuIcon");
-
-  function toggleMenu() {
-    if (menu.classList.contains("showMenu")) {
-      menu.classList.remove("showMenu");
-      closeIcon.style.display = "none";
-      menuIcon.style.display = "block";
-    } else {
-      menu.classList.add("showMenu");
-      closeIcon.style.display = "block";
-      menuIcon.style.display = "none";
-    }
-  }
-
-  hamburger.addEventListener("click", toggleMenu);
-
-  menuItems.forEach(function (menuItem) {
-    menuItem.addEventListener("click", toggleMenu);
+    $("#nav").toggleClass("active");
   });
 });
